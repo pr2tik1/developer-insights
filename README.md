3a37a87651c4624ec3b8dae72450582f65fdbe1d
pr2tik1 <pr2tik1@gmail.com>
2020-05-27 18:02:42 +0530

    add files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# developer-insights
-Exploratory Data Analysis using Python
+# Developer Insights for the year 2018-19
+
+Exploratory Data Analysis using python programming language. The article can be followed [here]().
+
+## Dependencies
+
+Libraries used,
+  - Scikit-Learn
+  - Numpy
+  - Pandas
+  - Matplotlib
+  - Seaborn
+  - Plolty-express
+
+
+## Usage 
+The notebooks and scripts can be used for reference and educational/research purposes, to install
+```
+Download directly the zip file 
+
+or type this in terminal,
+
+git clone https://github.com/pr2tik1/developer-survey.git
+```
+## Author
+Pratik Kumar : [LinkedIn](https://www.linkedin.com/in/pratik-kumar98/) 
+
---
+++ utils.py
@@ -0,0 +1,93 @@
+import pandas as pd 
+import plotly.express as px
+
+class data_handle():
+
+    def make_country_df(data1 , data2):
+        """
+        Returns a dataframe of each countries with number of developers for each year
+        """
+        df = pd.DataFrame()
+        
+        ############2019#########
+        country_data_1 = pd.DataFrame(data1.groupby('Country').MainBranch.agg('count'))
+        country_data_1.reset_index(inplace = True)
+        country_data_1.columns = ['Country', '2019']
+        ############2018#########
+        data2 = data2[data2['Student']=='No']
+        country_data_2 = pd.DataFrame(data2.groupby('Country').DevType.agg('count'))
+        country_data_2.reset_index(inplace = True)
+        country_data_2.columns = ['Country', '2018']
+        
+        #2017
+        #data3 = data3[data3['Professional']=='Professional developer']
+        #country_data_3 = pd.DataFrame(data3.groupby('Country').Professional.agg('count'))
+        #country_data_3.reset_index(inplace = True)
+        #country_data_3.columns = ['Country', '2017']
+        
+        ##############Inner joining############# 
+        temp = pd.merge(country_data_1 , country_data_2, on='Country')
+        #df = pd.merge(temp , country_data_3, on='Country')
+        return temp
+    
+    def make_dev(data_1, data_2):
+        """
+        Function to make dataframe for 2019 and 2018 data with unique Developer Types
+        Inputs : data_19 and data_18 dataframes 
+        Outputs : data18 and data19 dataframes with uniques rows of Developer types 
+        """
+        ###########2019#########
+        df1 = pd.DataFrame(data_1.set_index(data_1.columns.drop('DevType',1).tolist()).DevType.str.
+                           split(';', expand=True).stack().reset_index().rename(columns={0:'DevType'}).loc[:, data_1.columns])
+        ###########2018#########
+        df2 = pd.DataFrame(data_2.set_index(data_2.columns.drop('DevType',1).tolist()).DevType.str.
+                           split(';', expand=True).stack().reset_index().rename(columns={0:'DevType'}).loc[:, data_2.columns])
+
+        return df1, df2
+    
+    
+    def plot_missing(self,png=False):
+        """
+        Function to plot Missing data in the dataframe
+        Input: dataframe
+        Output: Bar-Plot
+        """
+        missing_data = self.isnull().sum()
+        missing_df = pd.DataFrame(missing_data.drop(missing_data[missing_data == 0].index).sort_values(ascending=False),
+                                 columns = ['values'])
+        fig = px.bar(data_frame = missing_df, x = missing_df.index, y = missing_df.values, text =missing_df.values,
+                  title = "Missing values count")
+        if png:
+            fig.show('png')
+        else:
+            fig.show()
+            
+            
+    def make_lang(data_1, data_2):
+        """
+        Function to make dataframe for 2019 and 2018 data with unique Languages 
+        Inputs : data_19 and data_18 dataframes 
+        Outputs : data18 and data19 dataframes with uniques rows of Language  
+        """
+        ###########2019#########
+        df1 = pd.DataFrame(data_1.set_index(data_1.columns.drop('LanguageWorkedWith',1).tolist()).LanguageWorkedWith.str.
+                           split(';', expand=True).stack().reset_index().rename(columns={0:'LanguageWorkedWith'}).loc[:, data_1.columns])
+        ###########2018#########
+        df2 = pd.DataFrame(data_2.set_index(data_2.columns.drop('LanguageWorkedWith',1).tolist()).LanguageWorkedWith.str.
+                           split(';', expand=True).stack().reset_index().rename(columns={0:'LanguageWorkedWith'}).loc[:, data_2.columns])
+        return df1, df2
+    
+    
+    def make_edu(data_1, data_2):
+        """
+        Function to make dataframe for 2019 and 2018 data with unique Languages 
+        Inputs : data_19 and data_18 dataframes 
+        Outputs : data18 and data19 dataframes with uniques rows of Language  
+        """
+        ###########2019#########
+        df1 = pd.DataFrame(data_1.set_index(data_1.columns.drop('EduOther',1).tolist()).EduOther.str.
+                           split(';', expand=True).stack().reset_index().rename(columns={0:'EduOther'}).loc[:, data_1.columns])
+        ###########2018#########
+        df2 = pd.DataFrame(data_2.set_index(data_2.columns.drop('EducationTypes',1).tolist()).EducationTypes.str.
+                           split(';', expand=True).stack().reset_index().rename(columns={0:'EducationTypes'}).loc[:, data_2.columns])
+        return df1, df2
